8179eabdab26fc05cd6e335ae2d3ea317ab66c2e
crescendonow <manowwon@gmail.com>
2026-05-08 11:13:43 +0700

    Fix frontend API proxy config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,10 @@
 set -eu
 
 : "${PORT:=8080}"
-: "${GO_GATEWAY_URL:=}"
+: "${GO_GATEWAY_URL:?GO_GATEWAY_URL is required. Set it to the public go-gateway URL, for example https://your-go-gateway.up.railway.app}"
+GO_GATEWAY_URL="${GO_GATEWAY_URL%/}"
 
 printf 'window.BLDG_API_BASE = "%s";\n' "$GO_GATEWAY_URL" > /usr/share/nginx/html/config.js
-envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${PORT} ${GO_GATEWAY_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
